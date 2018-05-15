cc3fbe3b9471dd458887840929d7aeb28757f97d
Your Name <you@example.com>
2018-05-15 11:22:34 +0800

    Tree_Diameter AC

--- p3w3/tree_Diameter/tree_Diameter.cpp
+++ p3w3/tree_Diameter/tree_Diameter.cpp
@@ -1,45 +1,70 @@
 #include "bits/stdc++.h"
 using namespace std;
+queue<int> q;
+int degree[100007];
+int level[100007];
+std::vector<int> G[100007];
 int main(int argc, char const *argv[]) {
-  // freopen("graph_diameter.in","r",stdin);
   int nCase;
   int num;
   cin>>nCase;
-  int parent[100000];
-  int len[100000];
-  bool is_par[100000];
   while (nCase--) {
-      cin>>num;
-      // cout<<num<<"AA"<<endl;
-      int par,chi;
-      memset(parent,0,sizeof(parent));
-      memset(is_par,0,sizeof(is_par));
-      memset(len,0,sizeof(len));
-      for(int i=0; i<num; i++) parent[i]=i;
-      for(int i=0; i<num-1; i++){
-        cin>>par>>chi;
-        parent[chi]=par;
-        is_par[par]=true;
-      }
-      cout<<"AAAAAAAAAAAAAAAAAAAA"<<endl;
-      int h1=-1e9,h2=-1e9;
-      for(int i=0; i<num; i++){
-        if(!is_par[i]){
-          int t=i;
-          int l=0;
-          while (parent[t]!=t) {
-            l++;
-            t=parent[t];
+    memset(degree,0,sizeof(degree));
+    memset(level,0,sizeof(level));
+    cin>>num;
+    for(int i=0; i<num; i++) G[i].clear();
+
+    int a,b;
+    for(int i=0; i<num-1; i++){
+      cin>>a>>b;
+
+      G[a].push_back(b);
+      G[b].push_back(a);
+      degree[a]++;
+      degree[b]++;
+
+    }
+    for(int i=0; i<num; i++){
+      if(degree[i]==1)
+        q.push(i);
+    }
+    int maxlevel=-1e9;
+    while (!q.empty()) {
+      int v = q.front(); q.pop();
+      for(int i=0; i<G[v].size(); i++ ){
+          int t=G[v][i];
+          degree[t]--;
+          if(degree[t]==1){
+            q.push(t);
+            level[t]=level[v]+1;
+            maxlevel=max(maxlevel,level[t]);
           }
-          cout<<i<<" "<<l<<endl;
-          if(l>h1){
-            h2=h1;
-            h1=l;
-          }else if(l>h2) h2=l;
-        }
       }
-      cout<<h1+h2;
-    if(nCase)puts("");
+    }
+    int c=0;
+    for(int i=0; i<num; i++){
+      if(level[i]==maxlevel) c++;
+    }
+    // diameter(G)=level(G)*2+|c|-1
+    cout<<maxlevel*2+c-1<<endl;
   }
   return 0;
 }
+/*
+2
+8
+0 1
+0 2
+0 3
+7 0
+1 4
+1 5
+3 6
+4
+0 1
+0 2
+2 3
+output
+4
+3
+*/
