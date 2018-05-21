a23df60c3a9e16b6ab93ff778715bf104fe6fab3
Your Name <you@example.com>
2018-05-21 12:18:18 +0800

    Bug array size too small

--- p3w4/Max_Min_brnach_nQueen/Max_Min_brnach_nQueen.cpp
+++ p3w4/Max_Min_brnach_nQueen/Max_Min_brnach_nQueen.cpp
@@ -1,15 +1,15 @@
 #include "bits/stdc++.h"
 using namespace std;
-int w[8][8];
-int q[8];
+long long w[20][20];
+int q[20];
 int n;
-int max_num,min_num;
+long long max_num,min_num;
 int num=0;
-void qn(int cur,int num){// (q[j],j) (i,cur)
+void qn(int cur,long long num){// (q[j],j) (i,cur)
   if(cur<n){
     for(int i=0; i<n; i++){
       for(int j=0; j<cur; j++){
-        if(q[j]==i) break; // 判斷是否在同一列
+        if(q[j]==i) break; // 判斷是否在同一行
         if(((q[j]-i)==(cur-j))||((q[j]-i)==(j-cur))) break; //是否在對角線
         if(j==cur-1){
           q[cur]=i;
@@ -23,20 +23,23 @@ void qn(int cur,int num){// (q[j],j) (i,cur)
   }
 }
 int main(int argc, char const *argv[]) {
+  freopen("Branch_8Queen.in","r",stdin);
   int nCase;
   cin>>nCase;
   while (nCase--) {
     cin>>n;
-    max_num=0;
+    max_num=-1e9;
     min_num=1e9;
     for(int i=0; i<n; i++)
-      for(int j=0; j<n; j++) cin>>w[i][j];
+      for(int j=0; j<n; j++){
+        cin>>w[i][j];
+      }
 
   for(int i=0; i<n; i++){
     q[0]=i;
     qn(1,w[0][i]);
   }
-  cout<<(max_num!=0?max_num:-1);
+  cout<<(max_num!=-1e9?max_num:-1);
   cout<<" "<<(min_num!=1e9?min_num:-1);
     if(nCase) puts("");
   }
